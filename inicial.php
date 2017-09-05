6b26ae3d4b8f77c7a9833f3a5e06e9e513b1e8df
miquelperez <31473831+miquelperez@users.noreply.github.com>
2017-09-05 11:28:28 +0200

    Add files via upload

---
+++ inicial.php
@@ -0,0 +1,148 @@
+<!--Mask-->
+<div class="view hm-black-strong">
+    <div class="full-bg-img flex-center">
+        <ul class="animated fadeInUp">
+            <li>
+                <h1 class="h1-responsive">Material Design for Bootstrap 4</h1></li>
+            <li>
+                <p>The most powerful and free UI KIT for Bootstrap</p>
+            </li>
+            <li>
+                <a target="_blank" href="http://mdbootstrap.com/getting-started/" class="btn btn-primary btn-lg" rel="nofollow">Sign up!</a>
+                <a target="_blank" href="http://mdbootstrap.com/material-design-for-bootstrap/" class="btn btn-default btn-lg" rel="nofollow">Learn more</a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+<div class="container">
+<!--/.Mask-->
+<div class="divider-new">
+    <h2 class="h2-responsive wow fadeIn">Best features</h2>
+</div>
+
+<!--Section: Best features-->
+<section id="best-features">
+
+    <div class="row">
+
+        <!--First columnn-->
+        <div class="col-lg-3">
+            <!--Card-->
+            <div class="card wow fadeIn">
+
+                <!--Card image-->
+                <div class="view overlay hm-white-slight">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20(25).jpg" class="img-fluid" alt="">
+                    <a>
+                        <div class="mask"></div>
+                    </a>
+                </div>
+                <!--/.Card image-->
+
+                <!--Card content-->
+                <div class="card-block text-center">
+                    <!--Title-->
+                    <h4 class="card-title">360 Advertising</h4>
+                    <hr>
+                    <!--Text-->
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident fuga animi architecto dolores dicta cum quo velit.</p>
+                </div>
+                <!--/.Card content-->
+
+            </div>
+            <!--/.Card-->
+        </div>
+        <!--First columnn-->
+
+        <!--Second columnn-->
+        <div class="col-lg-3">
+            <!--Card-->
+            <div class="card wow fadeIn" data-wow-delay="0.2s">
+
+                <!--Card image-->
+                <div class="view overlay hm-white-slight">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20(14).jpg" class="img-fluid" alt="">
+                    <a>
+                        <div class="mask"></div>
+                    </a>
+                </div>
+                <!--/.Card image-->
+
+                <!--Card content-->
+                <div class="card-block text-center">
+                    <!--Title-->
+                    <h4 class="card-title">Top-class Team</h4>
+                    <hr>
+                    <!--Text-->
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident fuga animi architecto dolores dicta cum quo velit.</p>
+                </div>
+                <!--/.Card content-->
+
+            </div>
+            <!--/.Card-->
+        </div>
+        <!--Second columnn-->
+
+        <!--Third columnn-->
+        <div class="col-lg-3">
+            <!--Card-->
+            <div class="card wow fadeIn" data-wow-delay="0.4s">
+
+                <!--Card image-->
+                <div class="view overlay hm-white-slight">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20(21).jpg" class="img-fluid" alt="">
+                    <a>
+                        <div class="mask"></div>
+                    </a>
+                </div>
+                <!--/.Card image-->
+
+                <!--Card content-->
+                <div class="card-block text-center">
+                    <!--Title-->
+                    <h4 class="card-title">Newest Technolgies</h4>
+                    <hr>
+                    <!--Text-->
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident fuga animi architecto dolores dicta cum quo velit.</p>
+                </div>
+                <!--/.Card content-->
+
+            </div>
+            <!--/.Card-->
+        </div>
+        <!--Third columnn-->
+
+        <!--First columnn-->
+        <div class="col-lg-3">
+            <!--Card-->
+            <div class="card wow fadeIn" data-wow-delay="0.6s">
+
+                <!--Card image-->
+                <div class="view overlay hm-white-slight">
+                    <img src="https://mdbootstrap.com/img/Photos/Horizontal/Work/4-col/img%20(37).jpg" class="img-fluid" alt="">
+                    <a>
+                        <div class="mask"></div>
+                    </a>
+                </div>
+                <!--/.Card image-->
+
+                <!--Card content-->
+                <div class="card-block text-center">
+                    <!--Title-->
+                    <h4 class="card-title">24/7 Support</h4>
+                    <hr>
+                    <!--Text-->
+                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident fuga animi architecto dolores dicta cum quo velit.</p>
+                </div>
+                <!--/.Card content-->
+
+            </div>
+            <!--/.Card-->
+        </div>
+        <!--First columnn-->
+    </div>
+
+</section>
+<!--/Section: Best features-->
+</div>
